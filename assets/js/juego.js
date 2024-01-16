7a327aea1e883318a7af6669da72bbb1c023d5b9
RB <87038824+MrMhirs@users.noreply.github.com>
2024-01-16 14:43:48 +0100

    CreateDeck

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,1 +1,16 @@
 // 2C = two of Clubs
+
+let deck = [];
+const types = ["C", "D", "H", "S"];
+
+const createDeck = () => {
+  for (let i = 2; i <= 10; i++) {
+    for (let type of types) {
+      deck.push(i + type);
+    }
+
+    // deck.push(i + "C");
+  }
+  console.log(deck);
+};
+createDeck();
