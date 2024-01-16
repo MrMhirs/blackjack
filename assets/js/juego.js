7888a40d0ba6188448523b704926569f45e15a15
RB <87038824+MrMhirs@users.noreply.github.com>
2024-01-16 14:47:34 +0100

    Finiched cards and func

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -2,14 +2,20 @@
 
 let deck = [];
 const types = ["C", "D", "H", "S"];
+const specials = ["A", "J", "Q", "K"];
 
 const createDeck = () => {
   for (let i = 2; i <= 10; i++) {
     for (let type of types) {
       deck.push(i + type);
     }
-
     // deck.push(i + "C");
+  }
+
+  for (let type of types) {
+    for (let esp of specials) {
+      deck.push(esp + type);
+    }
   }
   console.log(deck);
 };
