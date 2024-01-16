814a6c9778fdadf531b864c24b372fe1b8ca6edd
RB <87038824+MrMhirs@users.noreply.github.com>
2024-01-16 15:34:51 +0100

    spliced issue

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,6 +1,7 @@
 // 2C = two of Clubs
 
 let deck = [];
+let deckWithOutCard = [];
 const types = ["C", "D", "H", "S"];
 const specials = ["A", "J", "Q", "K"];
 
@@ -11,12 +12,37 @@ const createDeck = () => {
     }
     // deck.push(i + "C");
   }
-
   for (let type of types) {
     for (let esp of specials) {
       deck.push(esp + type);
     }
   }
+  //   console.log(deck);
+  deck = _.shuffle(deck);
+
   console.log(deck);
 };
 createDeck();
+
+
+
+
+
+
+const askCard = () => {
+
+    let card = deck.pop();
+    let deckWithOutCard = () => {
+        for (let i = 0; index < deck.length; index++) {
+            if (deck[i] === card){
+                let spliced = deck.splice(i, 1)
+            }
+        }
+    }
+
+  console.log(card);
+  console.log(deck);
+  console.log(deckWithOutCard);
+};
+
+askCard();
